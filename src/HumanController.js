9da8f85d1ccebb86ff3d57b29d0c26bb776bf069
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2018-01-28 02:06:01 +0100

    bug fixes

--- src/HumanController.js
+++ src/HumanController.js
@@ -8,7 +8,9 @@ export default class HumanController extends Script {
       ...Script.propsTypes,
       area: 'array(number)',
       speedWalk: 'number',
-      speedRun: 'number'
+      speedRun: 'number',
+      yRange: 'number',
+      speedRandomOffset: 'number'
     };
   }
 
@@ -22,6 +24,8 @@ export default class HumanController extends Script {
     this.area = [-100, 100];
     this.speedWalk = 100;
     this.speedRun = 200;
+    this.yRange = 0;
+    this.speedRandomOffset = 0;
     this._direction = Math.random() > 0.5 ? 1 : -1;
     this._isRunning = false;
   }
@@ -35,8 +39,14 @@ export default class HumanController extends Script {
   onAttach() {
     super.onAttach();
 
-    const { area } = this;
-    this.entity.setPosition(randomRange(area[0], area[1]), 0);
+    const { area, yRange, speedRandomOffset } = this;
+    this.entity.setPosition(
+      randomRange(area[0], area[1]),
+      randomRange(-yRange, yRange)
+    );
+
+    this.speedWalk += speedRandomOffset * (Math.random() * 2 - 1);
+    this.speedRun += speedRandomOffset * (Math.random() * 2 - 1);
 
     System.events.trigger('register-human', this);
   }
